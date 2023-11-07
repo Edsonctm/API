d01fad496051db1eb1d452298e27d8da340a2757
edson <edson_ctm@hotmail.com>
2023-11-06 22:48:31 -0300

    bootstrap

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  teste: string
+  urlImagem = "https://boomba.club/sex/uploads/posts/2022-11/1667881829_boomba-club-p-alexis-texas-anus-erotika-vkontakte-3.jpg"
+
+  constructor(private loginService: LoginService){
+
+    this.teste = loginService.funcaoTeste()
+
+  }
 
 }
